031a68bd1dbe6a1cf7ce8b053f9dd0465d04e796
Anh Mai Kieu Nguyen <anhmaikieunguyen@gmail.com>
2023-03-26 14:31:11 -0400

    hw06 starter

---
+++ hw06/main.cpp
@@ -0,0 +1,33 @@
+// #define COW_PATCH_FRAMERATE
+// #define COW_PATCH_FRAMERATE_SLEEP
+#include "include.cpp"
+
+
+
+void hw6a() {
+
+}
+
+
+
+void hw6b() {
+
+}
+
+
+
+void hw6c() {
+
+}
+
+
+
+int main() {
+    APPS {
+        APP(hw6a);
+        APP(hw6b);
+        APP(hw6c);
+    }
+    return 0;
+}
+
